dff49b8f652201c7b6cefebb0acdf22e61224e96
Onur Salk <salk.onur@gmail.com>
2016-04-29 08:05:22 +0300

    Modified web template

--- hello.py
+++ hello.py
@@ -3,10 +3,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html',title='Home')
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run()
